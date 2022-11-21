b49deee8e8599b508245c57a7f258efdcadfeaf7
SSabarinathan <ssabarinathan200109@gmail.com>
2022-11-21 22:18:56 +0530

    css framings, events, multimedia query

---
+++ touch_events.js
@@ -0,0 +1,13 @@
+document.addEventListener("touchstart", e=>{
+    console.log("start");
+})
+
+document.addEventListener("touchmove", e=>{
+    console.log("move");
+})
+
+document.addEventListener("touchend", e=>{
+    console.log("end");
+})
+
+
